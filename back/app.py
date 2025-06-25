43f7efd537d3e4b488a5391b2c2189676213da58
Venkata Sandeep Pesala <87456729+Ignitoz@users.noreply.github.com>
2025-06-25 18:08:14 +0530

    Add files via upload

--- back/app.py
+++ back/app.py
@@ -13,333 +13,163 @@ from zoneinfo import ZoneInfo
 from twilio.rest import Client
 import time
 import pymongo
+from dateutil.relativedelta import relativedelta
+
+
 
 app = Flask(__name__)
-CORS(app)  # Allows requests from all origins (React frontend)
-  # Twilio's sandbox number (or your purchased number)
-# MongoDB connection (replace with your actual credentials)
+CORS(app)
+account_sid = 'your_account_sid'  # Find in Twilio Console
+auth_token = 'your_auth_token'    # Find in Twilio Console
+twilio_whatsapp_number = 'whatsapp:+14155238886'
+Client = Client(account_sid, auth_token)
+mongo_uri = os.getenv("MONGO_URI")
 
-mongo_uri=os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri)
 db = client.users
 collection = db.customers
 
-BASE62_ALPHABET = string.digits + string.ascii_uppercase
 
 
-def generate_secure_id(firstName, lastName):
-    year_suffix = str(datetime.now().year)[-2:]
-    initials = firstName[0].upper() + lastName[0].upper()
+def calculate_penalty(start_date, end_date, monthly_penalty=500):
+    if end_date < start_date:
+        return 0  # or raise error if needed
 
-    prefix = f"MF{year_suffix}{initials}"
+    delta = end_date - start_date
 
-    # Get the latest ID with the same prefix
-    last_entry = collection.find_one(
-        {"unique_id": {"$regex": f"^{prefix}"}},
-        sort=[("unique_id", -1)]
-    )
+    # Rule: No penalty if <= 5 days
+    if delta.days <= 5:
+        return 0
 
-    if last_entry:
-        last_seq = int(last_entry["unique_id"][-5:])
-        new_seq = last_seq + 1
-    else:
-        new_seq = 1
+    # Else calculate months + 1 if any extra days
+    diff = relativedelta(end_date, start_date)
+    months = diff.years * 12 + diff.months
 
-    new_id = f"{prefix}{new_seq:05d}"  # pad sequence with zeros
-    return new_id
+    # Add one more month if there's any remaining days
+    if diff.days > 0:
+        months += 1
 
+    return months * monthly_penalty, months
 
-def is_duplicate_customer(hpNumber,aadhaarOrPan,  email=None, phone=None):
-    reasons = []
-    # Build the query to match existing customers with same details
-    if collection.find_one({"aadhaarOrPan": aadhaarOrPan}):
-        reasons.append("aadhaarOrPan already exists")
-    # Optionally add more fields to check for better accuracy 
-    if email and collection.find_one({"email": email}):
-        reasons.append("email already exists")
+def calculate_months_overdue(due_date, current_date,GRACE_PERIOD_DAYS):
+    if current_date < due_date + timedelta(days=GRACE_PERIOD_DAYS):
+        return 0
+    diff_years = current_date.year - due_date.year
+    diff_months = current_date.month - due_date.month
+    total_months = (diff_years * 12) + diff_months
+    if current_date.day < due_date.day:
+        total_months -= 1
+    return max(0, total_months)
 
-    if phone and collection.find_one({"phoneNumber": phone}):
-        reasons.append("phoneNumber already exists")
+@app.route("/remainder_whatsapp", methods=['GET'])
+def remainder():
+    remainder = db.repayments
+    now_ist = datetime.now()
+    three_days_later = now_ist + timedelta(days=3)
 
-    if hpNumber and collection.find_one({"hpNumber": hpNumber}):
-        reasons.append("hpNumber already exists")
-
-    if reasons:
-        return True, ", ".join(reasons)
-    return False, None
-
-
-def generate_loan_id(customer_id):
-    loans =db.loans
-    while True:
-        rand_int1 = str(random.randint(10**3, 10**4 - 1))
-        rand_int2 = str(random.randint(10**2, 10**3 - 1))
-        loan_id = f"LN{rand_int1}{customer_id[:3]}{rand_int2}"
-        if not loans.find_one({"loanId":loan_id}):
-            return loan_id
-        
-def create_repayment_schedule(loan_id, customer_id, months,emi):
-    start_date = datetime.now(ZoneInfo("Asia/Kolkata"))
-    try:
-        for i in range(int(months)):
-            due_date = start_date + timedelta(days=30 * (i + 1))  # Approx 1 month intervals
-            print(i)
-            entry = {
-                "loanId": loan_id,
-                "hpNumber": customer_id,
-                "installmentNumber": i + 1,
-                "dueDate": due_date,
-                "amountDue": emi,
-                "amountPaid": 0,
-                "status": "pending",
-                "paymentDate": None,
-                "paymentId":None,
-                "paymentMode":None,
-                "penalty":0,
-                "totalAmountDue":emi,
-                "updatedOn":None
-                        }
-            result = db.repayments.insert_one(entry)
-            print(result)
-    except Exception as e:
-        return {"status":"error","message":str(e)}
-    
-
-
-def insert_customer(customer_data):
-    is_duplicate, reason = is_duplicate_customer(
-        customer_data.get("hpNumber"),
-        customer_data.get("aadhaarOrPan"),
-        customer_data.get("email"),
-        customer_data.get("phoneNumber"),
-    )
-    if is_duplicate:
-        return jsonify({"status": "error", "message": f"Duplicate found: {reason}"}), 409
-    else:
-        unique_number = generate_secure_id(customer_data["firstName"],customer_data["lastName"])
-        customer_data["hpNumber"]= unique_number
-        customer_data["InsertedOn"]= datetime.now(ZoneInfo("Asia/Kolkata"))
-        result = collection.insert_one(customer_data)
-        customer_data.pop("_id",None)
-        return {"insert_id": str(result.inserted_id),"data":customer_data}
-
-def total_loan_payable(loanId):
-    pipeline = [
-                {"$match": {"loanId": loanId}},  # Filter by loanId
-                {"$group": {
-                    "_id": "$loanId",
-                    "total_payable": {"$sum": { "$toDouble": "$totalAmountDue" }}
-                }}
-            ]
-    result = list(db.repayments.aggregate(pipeline))
-    old_total = float(db.loans.find_one({"loanId":loanId},{"totalPayable":1})["totalPayable"])
-    new_total = round(float(result[0]["total_payable"]),1)
-    if new_total>old_total:
-        db.loans.update_one({"loanId":loanId},{"$set":{"totalPayable":str(new_total)}})
-        return str(new_total)
-    else:
-        return str(old_total)   
-
-
-def insert_loan_data(loan_data):
-    #print(loan_data)
-    if "hpNumber" in loan_data.keys():
-        loan_data["loanId"] = generate_loan_id(loan_data.get("hpNumber"))
-        result = db.loans.insert_one(loan_data)
-        loan_data.pop("_id",None)
-        return {"data":loan_data}
-    else:
-        return {"error":"hpNumber was not sent"}
-    
-
-def generate_unique_payment_id():
-    # Get last 8 digits of current milliseconds
-    ts_part = str(int(time.time() * 1000))[-8:]
-    # Add 3 random digits
-    rand_part = str(random.randint(100, 999))
-    return ts_part + rand_part
-
-
-def serialize_doc(doc):
-    doc["_id"] = str(doc["_id"])
-    return doc
-
-
-
-@app.route('/submit', methods=['POST'])
-def submit_data():
-    try:
-        data = request.get_json(force=True)
-        res = insert_customer(data)
-        #print(result)
-        if "error" in res:
-            return jsonify({"status": "error", "message": res["error"]}), 400
-
-        return jsonify({"status": "success", "response":res["data"]}), 200
-
-    except Exception as e:
-        
-        traceback.print_exc()  # prints full error traceback to logs
-        return jsonify({"status": "error", "message": str(e)}), 500
-
-@app.route("/customers", methods=["GET"])
-def get_all_customers():
-    customers = list(collection.find())
-    serialized_customers = [serialize_doc(doc) for doc in customers]
-    return jsonify({"customers_data":serialized_customers,"status":"success"}),200
-
-@app.route("/only_customer_and_loans",methods=["GET"])
-def get_all_customers_loans():
-    customers = list(collection.find())
-    serialized_customers = [serialize_doc(doc) for doc in customers]
-    for customer in serialized_customers:
-        loans = list(db.loans.find({"hpNumber":customer["hpNumber"]}))
-        serialized_loans = [serialize_doc(doc) for doc in loans]
-        customer["loans"]= serialized_loans
-    return jsonify({"customers_data":serialized_customers,"status":"success"}),200
-
-
-@app.route("/loan", methods=["POST"])
-def submit_loan():
-    try:
-        data = request.get_json(force=True)
-        res = insert_loan_data(data)
-        
-        #print(result)
-        if "error" in res:
-            return jsonify({"status": "error", "message": res["error"]}), 400
-        res = res.get("data")
-        #print(res)
-        create_repayment_schedule(res["loanId"], res["hpNumber"], res["loanTerm"],res["monthlyEMI"])
-        return jsonify({"status": "success", "response":res,"addInfo":"Repayment DB updated"}), 200
-
-    except Exception as e:
-        
-        traceback.print_exc()  # prints full error traceback to logs
-        return jsonify({"status": "error", "message": str(e)}), 500
-    
-@app.route('/get_customer_loan_info', methods=['POST'])
-def get_customer_loans():
-    data = request.get_json(force=True)
-    
-
-    if not "hpNumber" in data.keys():
-        return jsonify({"error": "customer_id is required"}), 400
-    customer_id = data.get("hpNumber")
-    pipeline = [
-        { "$match": { "hpNumber": customer_id } },
-        {
-            "$lookup": {
-                "from": "loans",
-                "localField": "hpNumber",
-                "foreignField": "hpNumber",  # or "_id" if customer ID is MongoDB ObjectId
-                "as": "loanInfo"
-            }
-        },
+    # Query repayments due in next 3 days and not paid
+    repayments = remainder.find({
+        "status": {"$ne": "paid"},
+        "$or": [
+        {"dueDate": {"$gte": now_ist, "$lte": three_days_later}},
+        {"dueDate": {"$lt": now_ist}}
     ]
-    def convert_objectids(doc):
-        if isinstance(doc, list):
-            return [convert_objectids(d) for d in doc]
-        elif isinstance(doc, dict):
-            return {k: convert_objectids(v) for k, v in doc.items()}
-        elif isinstance(doc, ObjectId):
-            return str(doc)
-        return doc
+    })
+    for repayment in repayments:
+        customer_id = repayment.get("hpNumber")
+        if not customer_id:
+            continue
+        user = collection.find_one({"hpNumber": customer_id})
+        if not user:
+            continue
+        name = user.get("firstName") + " " + user.get("lastName")
+        phone = "+91"+str(user.get("phone"))  # Must be in +91XXXXXXXXXX format
+        if not phone:
+            continue
+        amount = repayment.get("totalAmountDue", 0)
+        due_date = repayment.get("dueDate")+timedelta(hours=5,minutes=30)
+        due_date_str = due_date.strftime("%d-%b-%Y")
+        message = (
+            f"Hi {name},\n\n"
+            f"This is a reminder that your loan repayment of ₹{amount} is due on {due_date_str}.\n"
+            "Please pay before the due date to avoid penalties.\n\nThank you!"
+        )
+        try:
+            # Schedule message 2 minutes from now (local time)
+            now_local = datetime.now(ZoneInfo("Asia/Kolkata"))
+            send_hour = now_local.hour
+            send_minute = now_local.minute + 2
+
+            if send_minute >= 60:
+                send_minute -= 60
+                send_hour = (send_hour + 1) % 24
+
+            print(f"Scheduling message to {phone} at {send_hour}:{send_minute}")
+            mess = client.messages.create(
+                body=message,
+                from_=twilio_whatsapp_number,
+                to=f'whatsapp:{phone}'  # Replace with recipient's number
+            )
+            #kit.sendwhatmsg(phone, message, send_hour, send_minute)
+            #time.sleep(2)  # wait to avoid too many rapid opens
+        except Exception as e:
+            return jsonify({f"Error sending message to {phone}": f"{e}"}),400
+    return jsonify({"status": "success","message":"Remainder messagges sent"}),200
 
 
-
-    result = list(db.customers.aggregate(pipeline))
-    return jsonify({"status":"Success","response":convert_objectids(result)})
-    
-
-@app.route('/get_customer_repayment_info', methods=['POST'])
-def get_repayment_info():
-    data = request.get_json(force=True)
-    
-
-    if not "loanId" in data.keys():
-        return jsonify({"error": "loan_id is required"}), 400
-    loan_id = data.get("loanId")
-    try:
-        results = db.repayments.find({"loanId": loan_id})
-        serialized_repayments = [serialize_doc(doc) for doc in results]
-        return jsonify({"repayment_data":serialized_repayments,"status":"success"}),200
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
-
-@app.route('/get_customer_loans', methods=['POST'])
-def get_loans_with_repayments():
-    try:
-        data = request.get_json(force=True)
-        hp_number = data.get("hpNumber")
-
-        if not hp_number:
-            return jsonify({"error": "hpNumber is required"}), 400
-        def serialize(doc):
-            doc["_id"] = str(doc["_id"])
-            return doc
-        loans = list(db.loans.find({"hpNumber": hp_number}))
-        customer_details = collection.find_one({"hpNumber": hp_number},{"firstName":1,"lastName":1,"phone":1,"_id":0})
-        #customer_seralized = serialize(customer_details)
-        # Serialize ObjectId and add repayments to each loan
-        
-
-        result = []
-        for loan in loans:
-            loan_serialized = serialize(loan)
-            total =total_loan_payable(loan["loanId"])
-            loan_serialized["totalPayable"]=total
-            result.append(loan_serialized)
-
-        return jsonify({
-            "status": "success",
-            "data": result,
-            "customerDetails":customer_details
-        })
-
-    except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
-    
-@app.route("/update_repayment",methods=["POST"])
-def update_repayment():
-    data = request.get_json(force=True)
-    required_keys ={"amountPaid", "status","paymentMode","recoveryAgent","totalAmountDue","loanId","installmentNumber"}
-    try:
-        if required_keys.issubset(data):
-            result = db.repayments.update_one( {
-                                                "loanId": data.get("loanId").strip(),
-                                                "installmentNumber": int(data.get("installmentNumber"))
-                                            },
-                                            {
-                                                "$set": {
-                                                    "amountPaid": data.get("amountPaid"),
-                                                    "status": data.get("status"),
-                                                    "paymentDate": datetime.now(ZoneInfo("Asia/Kolkata")),
-                                                    "paymentId": generate_unique_payment_id(),
-                                                    "paymentMode": data.get("paymentMode"),
-                                                    "recoveryAgent": data.get("recoveryAgent"),
-                                                    "totalAmountDue": data.get("totalAmountDue")
-                                                }
-                                            }
-                                        )
-            if result.matched_count == 0:
-                return jsonify({"status":"error","message":"No document matched the query. Check loanId or installmentNumber."}),400
-            elif result.modified_count == 0:
-                return jsonify({"status":"error","message":"Document matched but no fields were updated (values may be the same)."}),400
-            else:
-                return jsonify({"status":"success","message":"Repayment DB updated successfully"}),200
-
-        else:   
-            missing = required_keys - data.keys()
-            return jsonify({"status":"error","message":f"The following keys are missing: {missing}"}),404
-
-        
-    except Exception as e:
-        return jsonify({"status":"error","message":str(e)}),500
-    
 @app.route("/")
 def home():
     return jsonify({"message": "API is running"})
+
+
+@app.route("/update_penalty_new", methods=['GET'])
+def apply_monthly_penalties_new(): 
+    PENALTY_PER_MONTH = 300
+    GRACE_PERIOD_DAYS = 5
+    try:
+        remainder_collection = db.repayments
+        current_date = datetime.now(ZoneInfo("Asia/Kolkata"))
+        overdue_repayments = remainder_collection.find({
+            "status": {"$ne": "paid"},
+            "dueDate": {"$lt": current_date} # Initial filter: due date is in the past
+        })
+
+        bulk_updates = []
+        for repayment in overdue_repayments:
+            print(repayment)
+            repayment_id = repayment['_id']
+            due_date = repayment['dueDate'] # Assuming this is a datetime object from MongoDB
+            due_date = due_date.replace(tzinfo=ZoneInfo("Asia/Kolkata"))
+            print(current_date,due_date)
+            emi = float(repayment["amountDue"])
+            penalty,months = calculate_penalty(due_date,current_date)
+            bulk_updates.append(
+                {
+                    "filter": {"_id": repayment_id},
+                    "update": {
+                        "$set": {
+                            "updatedOn": current_date, # Set last update time
+                            "TotalPenaltyMonths": months,
+                            "penalty": penalty,
+                            "totalAmountDue": str(emi+penalty)
+                            }
+                    }
+                }
+            )
+        if bulk_updates:
+        # Execute bulk updates
+            req = []
+            for item in bulk_updates:
+                req.append(
+                    pymongo.UpdateOne(item["filter"], item["update"])
+                )
+            result = remainder_collection.bulk_write(req)
+            return jsonify({"status":"Success","message":f"Penalties updated for {result.modified_count}"}),200
+        else:
+            return jsonify({"status":"Success","message":"No records to update"}),200
+    except Exception as e:
+        return jsonify({"status":"error","message":str(e)})
+
 
 if __name__ == '__main__':
     app.run()
