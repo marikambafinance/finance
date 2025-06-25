b8cbb90f280a2f28d050a9f984d15c51046d763a
Venkata Sandeep Pesala <87456729+Ignitoz@users.noreply.github.com>
2025-06-25 19:04:19 +0530

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -19,8 +19,8 @@ CORS(app)  # Allows requests from all origins (React frontend)
   # Twilio's sandbox number (or your purchased number)
 # MongoDB connection (replace with your actual credentials)
 
-#mongo_uri=os.getenv("MONGO_URI")
-client = MongoClient("mongodb+srv://mariamma:0dkg0bIoBxIlDIww@cluster0.yw4vtrc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongo_uri=os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
 db = client.users
 collection = db.customers
 
