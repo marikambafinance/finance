32164dc8905de05c24a8af6eaf8de4a1af0b6c4a
Venkata Sandeep Pesala <87456729+Ignitoz@users.noreply.github.com>
2025-07-19 10:47:41 +0530

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -447,6 +447,9 @@ def insert_loan_data(loan_data):
         loan_data["loanId"] = generate_loan_id(loan_data.get("hpNumber"))
         loan_data["status"] = "active"
         loan_data["totalAmountDue"] =loan_data["totalPayable"]
+        loan_data["totalPenalty"]="0"
+        loan_data["penaltyPaid"]="0"
+        loan_data["initialTotalPay"]=loan_data["totalPayable"]
         result = db.loans.insert_one(loan_data)
         loan_data.pop("_id",None)
         db.ledger.insert_one({
@@ -504,7 +507,7 @@ def close_loan_if_fully_paid(loan_id, installment_number):
             "status": "error",
             "message": str(e)
         }
-
+"""
 @app.before_request
 def global_auth_check():
     exempt_routes = ['home']
@@ -517,7 +520,7 @@ def global_auth_check():
         api_key = hashlib.sha256(api_key.encode()).hexdigest()
     if not api_key or api_key != EXPECTED_API_KEY:
         return jsonify({'message': 'Unauthorized'}), 401
-
+"""
 @app.route("/search",methods=["POST","OPTIONS"])
 def search():
     data = request.get_json(force=True)
